763f25c2465d53b213d231f7fb2ebb8b738a96b7
Matt Panzer <panzer.matthew@gmail.com>
2020-02-01 18:43:39 -0500

    fix run_desktop.sh to work on Windows

--- gui_and_analytics/run_desktop.sh
+++ gui_and_analytics/run_desktop.sh
@@ -2,4 +2,4 @@
 # Start the Debian desktop
 # Will need to figure out USB devices in the future:
 # https://stackoverflow.com/questions/24225647/docker-a-way-to-give-access-to-a-host-usb-or-serial-device
-docker run -p 5900:5900 -e VNC_SERVER_PASSWORD=password -v "$(pwd)":/app capstone-solar-sailor:latest
+docker run -p 5900:5900 -e VNC_SERVER_PASSWORD=password -v "/$(pwd):/app" capstone-solar-sailor:latest
